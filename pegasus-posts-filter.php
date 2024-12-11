af887938f5e17b3091a6bcdd7d722f4b582cb594
Jim O'Brien <jim.obrien3@gmail.com>
2024-12-11 00:53:00 -0500

    updating pegasus-posts-filter plugin: usage page when not pegasus theme

--- pegasus-posts-filter.php
+++ pegasus-posts-filter.php
@@ -18,21 +18,98 @@ Domain Path: /languages
 	if ( ! defined( 'ABSPATH' ) ) {
 		exit;
 	}
-	
-	
+
+	function posts_filter_check_main_theme_name() {
+		$current_theme_slug = get_option('stylesheet'); // Slug of the current theme (child theme if used)
+		$parent_theme_slug = get_option('template');    // Slug of the parent theme (if a child theme is used)
+
+		//error_log( "current theme slug: " . $current_theme_slug );
+		//error_log( "parent theme slug: " . $parent_theme_slug );
+
+		if ( $current_theme_slug == 'pegasus' ) {
+			return 'Pegasus';
+		} elseif ( $current_theme_slug == 'pegasus-child' ) {
+			return 'Pegasus Child';
+		} else {
+			return 'Not Pegasus';
+		}
+	}
+
+	function pegasus_posts_filter_menu_item() {
+		if ( posts_filter_check_main_theme_name() == 'Pegasus' || posts_filter_check_main_theme_name() == 'Pegasus Child' ) {
+			//do nothing
+		} else {
+			//echo 'This is NOT the Pegasus theme';
+			add_menu_page(
+				"Posts Filter", // Page title
+				"Posts Filter", // Menu title
+				"manage_options", // Capability
+				"pegasus_posts_filter_plugin_options", // Menu slug
+				"pegasus_posts_filter_plugin_settings_page", // Callback function
+				null, // Icon
+				91 // Position in menu
+			);
+		}
+	}
+	add_action("admin_menu", "pegasus_posts_filter_menu_item");
+
+	function pegasus_posts_filter_plugin_settings_page() { ?>
+	    <div class="wrap pegasus-wrap">
+			<h1>Posts Filter Usage</h1>
+
+			<div>
+				<h3>Posts Filter Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[ajax_filter_posts per_page="1"］</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[ajax_filter_posts per_page="1"］'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
+
+		</div>
+	<?php
+	}
+
 	function pegasus_posts_assets() {
 		//wp_enqueue_script( 'tuts/js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/matchHeight.js', ['jquery'], null, true );
-		wp_localize_script( 
-			'tuts/js', 
-			'pegasus', 
+		wp_localize_script(
+			'tuts/js', // Handle
+			'pegasus', // Object name
 			array(
-				'nonce'    => wp_create_nonce( 'pegasus' ),
-				'ajax_url' => admin_url( 'admin-ajax.php' )
+				'nonce'    => wp_create_nonce( 'pegasus' ), // Create nonce which we later will use to verify AJAX request
+				'ajax_url' => admin_url( 'admin-ajax.php' ) // Admin AJAX url
 			)
 		);
 	}
 	add_action('wp_enqueue_scripts', 'pegasus_posts_assets', 100);
-	
+
 
 /**
  * AJAX filter posts by taxonomy term
@@ -70,7 +147,7 @@ function vb_filter_posts() {
         die(json_encode($response));
     endif;
 
-    if ($term == 'all-terms') : 
+    if ($term == 'all-terms') :
 
         $tax_qry[] = [
             'taxonomy' => $tax,
@@ -127,7 +204,7 @@ function vb_filter_posts() {
                 'found' => $qry->found_posts
             ];
 
-            
+
         else :
 
             $response = [
@@ -177,7 +254,7 @@ function vb_filter_posts_sc($atts) {
                 <div class="status"></div>
                 <div class="content"></div>
             </div>
-        
+
         <?php $result = ob_get_clean();
     endif;
 
